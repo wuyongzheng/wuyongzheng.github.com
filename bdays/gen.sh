44d288e7a29813f7d0f4cd2c1cfe55d8158dff22
Wu Yongzheng <wuyongzheng@gmail.com>
2013-12-03 12:01:58 +0800

    countdown, 6 digits

--- bdays/gen.sh
+++ bdays/gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -n '1,/^var numbers = ..;$/p' days.template.html | head -n -1 >days.html
-echo "var numbers = [" >>days.html
+sed -n '1,/^var thenumbers = ..;$/p' days.template.html | head -n -1 >days.html
+echo "var thenumbers = [" >>days.html
 sed -e 's/"/\\"/g' -e 's/^\(.*\)\t\(.*\)$/\t\1, "\2",/' numbers.txt >>days.html
 echo "];" >>days.html
-sed -n '/^var numbers = ..;$/,$p' days.template.html | tail -n +2 >>days.html
+sed -n '/^var thenumbers = ..;$/,$p' days.template.html | tail -n +2 >>days.html
